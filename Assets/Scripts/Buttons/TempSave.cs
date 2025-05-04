11e0b9414ab9369abb99492205c3825bcdd46d85
Brabados <rhysipod@gmail.com>
2025-05-05 00:25:05 +1000

    Changed save to work with multiple saves, need to do same for load

--- Assets/Scripts/Buttons/TempSave.cs
+++ Assets/Scripts/Buttons/TempSave.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TempSave : MonoBehaviour
 {
     public Map ToSave;
+    public InputField input;
 
     public void Press()
     {
-        
-        SaveLoadManager.SaveLoadInstance.SaveMapToJson(ToSave, Application.persistentDataPath + "/" + "save.json");
+        if (input.text != null || input.text != "" || input.text != " ")
+        {
+            SaveLoadManager.SaveLoadInstance.SaveMapToJson(ToSave, Application.persistentDataPath + "/" + input.text.ToString() + ".json");
+        }
     }
 }
